bf992eab7f27e9c17c15bf9d576c4075444c20c9
blackrainbowtest <araqelyan.aram.96@gmail.com>
2026-01-12 23:52:56 +0400

    feat: added game fight scene components

---
+++ Assets/Game/Battle/Data/EnemyData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Game.Battle
+{
+    [CreateAssetMenu(menuName = "Game/Battle/Enemy")]
+    public class EnemyData : ScriptableObject
+    {
+        [Header("Main")]
+        public string enemyName;
+        public Sprite icon;
+
+        [Header("Stats")]
+        public int maxHp;
+        public int attack;
+        // Добавляй новые поля по мере необходимости
+    }
+}
